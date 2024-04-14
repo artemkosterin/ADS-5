32fe24a368affa075bae28a8de712c733d1e1a54
artemkosterin <160392965+artemkosterin@users.noreply.github.com>
2024-04-14 20:03:09 +0300

    b117 Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -1,10 +1,48 @@
-// Copyright 2021 NNTU-CS
-#ifndef INCLUDE_TSTACK_H_
-#define INCLUDE_TSTACK_H_
+#ifndef TSTACK_H
+#define TSTACK_H
 
-template<typename T, int size>
+template <typename T, int size>
 class TStack {
-  // добавьте код стека
+private:
+    T *stackArray;
+    int topIndex;
+public:
+    TStack() {
+        stackArray = new T[size];
+        topIndex = -1;
+    }
+    
+    ~TStack() {
+        delete[] stackArray;
+    }
+    
+    void push(T value) {
+        if (topIndex < size - 1) {
+            topIndex++;
+            stackArray[topIndex] = value;
+        }
+    }
+    
+    void pop() {
+        if (topIndex >= 0) {
+            topIndex--;
+        }
+    }
+    
+    T top() const {
+        if (topIndex >= 0) {
+            return stackArray[topIndex];
+        }
+        throw std::out_of_range("Stack is empty");
+    }
+    
+    bool isEmpty() const {
+        return topIndex == -1;
+    }
+    
+    bool isFull() const {
+        return topIndex == size - 1;
+    }
 };
 
-#endif  // INCLUDE_TSTACK_H_
+#endif
