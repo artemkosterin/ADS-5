e0070904d81f485620697c8693fb3cd52aa41002
artemkosterin <160392965+artemkosterin@users.noreply.github.com>
2024-04-14 20:53:33 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -9,28 +9,26 @@ class TStack {
  private:
     T arr[100];
     int top;
-
  public:
     TStack(): top(-1) {}
-    void push(T value) {
-      if (!isFull())
-        arr[++top] = value;
+    void pushh(T val) {
+      if (!isOverflow())
+        arr[++top] = val;
       else
-        throw std::string("Full");
+        throw std::string("Stack overflow");
     }
-    T get() const {
-      return arr[top];
-    }
-    T pop() {
-      if (isEmpty())
-        throw std::string("Empty");
+    T popp() {
+      if (isEmptyy())
+        throw std::string("Stack is empty");
       else
         return arr[top--];
     }
-    bool isEmpty() const {
+    bool isOverflow() const {
+      return top == size - 1;
+    }
+    bool isEmptyy() const {
       return top == -1;
     }
-    bool isFull() const {
-           return top == size - 1;
-    }
 };
+
+#endif  // INCLUDE_TSTACK_H_
