b6c2cb866f096207c649c9953121ac1b4f783c84
artemkosterin <160392965+artemkosterin@users.noreply.github.com>
2024-04-14 20:18:57 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -1,28 +1,28 @@
 #ifndef TSTACK_H
 #define TSTACK_H
 
-template <typename T, int size>
+template<typename T, int size>
 class TStack {
-  private:
+ private:
     T* stackArray;
     int topIndex;
-  public:
+ public:
     TStack() : topIndex(-1) {
-      stackArray = new T[size];
+        stackArray = new T[size];
     }
     void pop() {
-      if (topIndex >= 0)
-        topIndex--;
+        if (topIndex >= 0)
+            topIndex--;
     }
     void push(T item) {
-      if (topIndex < size - 1)
-        stackArray[++topIndex] = item;
+        if (topIndex < size - 1)
+            stackArray[++topIndex] = item;
     }
     T get() const {
-      return stackArray[topIndex];
+        return stackArray[topIndex];
     }
     bool checkEmpty() const {
-      return topIndex == -1;
+        return topIndex == -1;
     }
 };
 
