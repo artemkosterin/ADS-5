c9bcf659059ca3827d259a65b4bc3c56713acb0d
artemkosterin <160392965+artemkosterin@users.noreply.github.com>
2024-04-14 20:58:40 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,96 +1,127 @@
+#include <string>
 #include <map>
 #include "tstack.h"
+int prioty(char);
+int schet(int, int, char);
+std::string infx2pstfx(std::string);
+int eval(std::string);
 
-int prior(char sim) {
-    switch (sim) {
-    case '(':
-        return 0;
-        break;
-    case ')':
-        return 1;
-        break;
-    case '+':
-        return 2;
-        break;
-    case '-':
-        return 2;
-        break;
-    case '*':
-        return 3;
-        break;
-    case '/':
-        return 3;
-        break;
-    default:
-        return 0;
-        break;
-    }
-}
 std::string infx2pstfx(std::string inf) {
-  // добавьте код
-  return std::string("");
-    TStack<char, 100> ownStack;
-    std::string res = "";
-    for (int i = 0; i < inf.size(); i++) {
-        if (isdigit(inf[i]) != 0) {
-            res += inf[i];
-        } else if (prior(inf[i]) == 2 || prior(inf[i]) == 3) {
-            res += " ";
-            if (ownStack.isEmpty() || prior(ownStack.get()) == 0 ||
-                prior(inf[i]) > prior(ownStack.get())) {
-                ownStack.push(inf[i]);
-            } else if (prior(inf[i]) <= prior(ownStack.get())) {
-                while (prior(inf[i]) <= prior(ownStack.get())) {
-                    res += ownStack.pop();
-                    res += " ";
+        TStack<char, 100> stack;
+    std::string line = "";
+    int flag = 0;
+    try {
+        for (char& n : inf) {
+            if ((n >= '0') && (n <= '9')) {
+                if (flag == 1) {
+                    line = line + " " + n;
+                    flag = 0;
+                } else {
+                    line = line + n;
                 }
-                ownStack.push(inf[i]);
+            } else {
+                if (n == '(') {
+                    stack.pushup(n);
+                } else {
+                    flag = 1;
+                    if (stack.IfZero()) {
+                        stack.pushup(n);
+                    } else {
+                        if (n == ')') {
+                            while (stack.ElemUp() != '(') {
+                                line = line + " " + stack.popback();
+                            }
+                            stack.popback();
+                        } else {
+                            if (prioty(n) > prioty(stack.ElemUp())) {
+                                stack.pushup(n);
+                            } else {
+                                while ((!stack.IfZero()) &&
+                                    (prioty(n) <= prioty(stack.ElemUp()))) {
+                                    line = line + " " + stack.popback();
+                                }
+                                stack.pushup(n);
+                            }
+                        }
+                    }
+                }
             }
-        } else if (prior(inf[i]) == 0) {
-            ownStack.push(inf[i]);
-        } else if (prior(inf[i]) == 1) {
-            while (prior(ownStack.get()) != 0) {
-                res += " ";
-                res += ownStack.pop();
-            }
-            ownStack.pop();
         }
+        while (!stack.IfZero()) {
+            line = line + " " + stack.popback();
+        }
+        return line;
     }
-    while (!ownStack.isEmpty()) {
-        res += " ";
-        res += ownStack.pop();
+    catch (std::string maliniya) {
+        return "Fall!";
     }
-    return std::string(res);
 }
-int calculate(const int a, const int b, const char oper) {
-    switch (oper) {
-    case '+':
-        return b + a;
-    case '-':
-        return b - a;
-    case '*':
-        return a * b;
-    case '/':
-        return b / a;
-    default:
-        break;
+
+int eval(std::string pref) {
+        std::string line = "";
+    TStack<int, 100> stack1;
+    int flag = 0;
+    try {
+        for (char& b : pref) {
+            if (flag == 0) {
+                if (('0' <= b) && (b <= '9')) {
+                    line += b;
+                } else {
+                    if (line == "") {
+                        int y = stack1.popback();
+                        int x = stack1.popback();
+                        int res = schet(x, y, b);
+                        stack1.pushup(res);
+                        flag = 1;
+                    } else {
+                        stack1.pushup(stoi(line));
+                        line = "";
+                    }
+                }
+            } else {
+                flag = 0;
+            }
+        }
+        return stack1.popback();
+    }
+    catch (std::string maliniya) {
+        return -1;
     }
     return 0;
 }
 
-int eval(std::string pref) {
-  // добавьте код
-  return 0;
-    TStack<int, 100> opStack2;
-    for (int i = 0; i < pref.size(); i++) {
-        if (isdigit(pref[i]) != 0) {
-            int num = pref[i] - '0';
-            opStack2.push(num);
-        } else if (prior(pref[i]) == 2 || prior(pref[i]) == 3) {
-            int a = opStack2.pop();
-            int b = opStack2.pop();
-            opStack2.push(calculate(a, b, pref[i]));
-        }
+int prioty(char znak) {
+    int pryoto = 0;
+    switch (znak) {
+    case '+':
+        return 2;
+    case '-':
+        return 2;
+    case '(':
+        return 0;
+    case '*':
+        return 3;
+    case '/':
+        return 3;
+    case ')':
+        return 1;
+    default:
+        break;
     }
-    return opStack2.pop();
+    return pryoto;
+}
+
+int schet(int x, int y, char znak) {
+    switch (znak) {
+    case '+':
+        return x + y;
+    case '-':
+        return x - y;
+    case '*':
+        return x * y;
+    case '/':
+        return x / y;
+    default:
+        return 0;
+    }
 }
