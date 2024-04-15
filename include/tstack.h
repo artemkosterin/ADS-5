d60f57830f24c9ffe1e92cd29ac9d80613fb48bf
artemkosterin <160392965+artemkosterin@users.noreply.github.com>
2024-04-15 21:14:48 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -1,48 +1,37 @@
-#ifndef TSTACK_H
-#define TSTACK_H
+// Copyright 2021 NNTU-CS
+#ifndef INCLUDE_TSTACK_H_
+#define INCLUDE_TSTACK_H_
+#include <string>
 
-template <typename T, int size>
+template<typename T, int size>
 class TStack {
-private:
-    T *stackArray;
-    int topIndex;
-public:
-    TStack() {
-        stackArray = new T[size];
-        topIndex = -1;
-    }
-    
-    ~TStack() {
-        delete[] stackArray;
-    }
-    
+ private:
+    T arr[100];
+    int top;
+
+ public:
+    TStack(): top(-1) {}
     void push(T value) {
-        if (topIndex < size - 1) {
-            topIndex++;
-            stackArray[topIndex] = value;
-        }
+      if (!isFull())
+        arr[++top] = value;
+      else
+        throw std::string("Full");
     }
-    
-    void pop() {
-        if (topIndex >= 0) {
-            topIndex--;
-        }
+    T get() const {
+      return arr[top];
     }
-    
-    T top() const {
-        if (topIndex >= 0) {
-            return stackArray[topIndex];
-        }
-        throw std::out_of_range("Stack is empty");
+    T pop() {
+      if (isEmpty())
+        throw std::string("Empty");
+      else
+        return arr[top--];
     }
-    
     bool isEmpty() const {
-        return topIndex == -1;
+      return top == -1;
     }
-    
     bool isFull() const {
-        return topIndex == size - 1;
+      return top == size - 1;
     }
 };
 
-#endif
+#endif  // INCLUDE_TSTACK_H_
